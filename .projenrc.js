25107cb060a60fe7286bfadb5908448fd9055540
Pahud <pahudnet@gmail.com>
2021-01-11 16:15:44 +0800

    init

---
+++ .projenrc.js
@@ -0,0 +1,20 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.83.0',
+  name: 'eks-mng-tags',
+  cdkDependencies: [
+    '@aws-cdk/aws-eks',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-iam',
+  ],
+  dependabot: false,
+});
+
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
+
+
+
+project.synth();
---
+++ src/main.ts
@@ -0,0 +1,78 @@
+import { App, Construct, Stack, StackProps, Fn } from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
+import * as ec2 from '@aws-cdk/aws-ec2';
+
+export class Demo extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
+
+    const cluster = new eks.Cluster(this, 'Cluster', {
+      vpc: getOrCreateVpc(this),
+      version: eks.KubernetesVersion.V1_18,
+      defaultCapacity: 0,
+    })
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'set -o xtrace',
+      `/etc/eks/bootstrap.sh ${cluster.clusterName}`,
+    );
+    const lt = new ec2.CfnLaunchTemplate(this, 'LaunchTemplate', {
+      launchTemplateData: {
+        imageId: new eks.EksOptimizedImage().getImage(this).imageId,
+        instanceType: new ec2.InstanceType('t3.small').toString(),
+        userData: Fn.base64(userData.render()),
+        tagSpecifications: [
+          {
+            resourceType: 'instance',
+            tags: [
+              { key: 'Name', value: 'MNG'},
+              { key: 'Foo', value: 'BAR' },
+            ]
+          }
+        ]
+      },
+    });
+    cluster.addNodegroupCapacity('extra-ng', {
+      launchTemplateSpec: {
+        id: lt.ref,
+        version: lt.attrDefaultVersionNumber,
+      },
+      desiredSize: 2,
+    });
+
+
+  }
+}
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    new Demo(this, 'Demo')
+
+    // define resources here...
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'my-stack-dev', { env: devEnv });
+// new MyStack(app, 'my-stack-prod', { env: prodEnv });
+
+app.synth();
+
+
+function getOrCreateVpc(scope: Construct): ec2.IVpc {
+  // use an existing vpc or create a new one
+  return scope.node.tryGetContext('use_default_vpc') === '1' ?
+    ec2.Vpc.fromLookup(scope, 'Vpc', { isDefault: true }) :
+    scope.node.tryGetContext('use_vpc_id') ?
+      ec2.Vpc.fromLookup(scope, 'Vpc', { vpcId: scope.node.tryGetContext('use_vpc_id') }) :
+      new ec2.Vpc(scope, 'Vpc', { maxAzs: 3, natGateways: 1 });
+}
